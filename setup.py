766c8d1f2e813993d7acf3643a685a7a34199e07
Raaj <raj0kumar00@gmail.com>
2024-01-11 09:05:29 +0530

    added external package

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ setup(
     name="fdk",
     version="0.1",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        "mycolorlogger==0.8"
+        ],
     entry_points={
         'console_scripts': [
             'fdk = fdk.fdk:main',
